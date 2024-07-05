8a21c5cbbc66a4b178e7313f4cc7ba62a8d46f29
Guido Sanchez <sanchez.guido@gmail.com>
2024-07-05 14:57:08 +0000

    Add package: xsens_mti630_bringup

---
+++ xsens_mti630_bringup/launch/mti630.launch.py
@@ -0,0 +1,27 @@
+from launch import LaunchDescription
+from launch.actions import SetEnvironmentVariable
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+from pathlib import Path
+
+def generate_launch_description():
+
+    ld = LaunchDescription()
+
+    # Set env var to print messages to stdout immediately
+    arg = SetEnvironmentVariable('RCUTILS_LOGGING_BUFFERED_STREAM', '1')
+    ld.add_action(arg)
+
+    parameters_file_path = Path(get_package_share_directory('xsens_mti630_bringup'), 'config', 'mti630.yaml')
+    xsens_mti_node = Node(
+            package='bluespace_ai_xsens_mti_driver',
+            executable='xsens_mti_node',
+            name='xsens_mti_node',
+            namespace='mti_630_8A1G6',
+            output='screen',
+            parameters=[parameters_file_path],
+            arguments=[]
+            )
+    ld.add_action(xsens_mti_node)
+
+    return ld
